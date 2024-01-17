5e667df6eb656801f201a5169d1b15d090356124
ShenghaoisYummy <hsupisces@hotmail.com>
2024-01-17 19:40:53 +1100

    shopping cart completed

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import styles from './App.module.css'
 import Robot from "./components/Robot"
 import robots from "./mockdata/robots.json"
+import logo from "./assests/images/logo.svg"
+import ShoppingCart from './components/ShoppingCart';
+
 
 function App() {
+
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>robots are so cool!</h1>
+        </div>
+        <ShoppingCart />
       <div className={styles.robotList}>
       {robots.map(robots => <Robot id={robots.id} name={robots.name} email={robots.email} />)}
       </div>
