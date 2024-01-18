9d0f67288d54f9f19eaf623affbe3311ea571104
ShenghaoisYummy <hsupisces@hotmail.com>
2024-01-18 14:03:32 +1100

    reconstruct app use useState and useEffect, added loading section when fetching data

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './App.module.css'
 import Robot from "./components/Robot"
-import robots from "./mockdata/robots.json"
 import logo from "./assests/images/logo.svg"
 import ShoppingCart from './components/ShoppingCart';
 
 interface Props {}
 
 interface State {
-  robotGallery: any;
+  robotGallery: any[];
 }
 
-class App extends React.Component<Props, State> {
+const App:React.FC  = () =>{
 
-  constructor(props:Props) {
-    super(props);
-      this.state = {
-        robotGallery: [],
-      };
+const [robotGallery, setRobotGallery] = useState<any>([]);
+const [isLoading, setisLoading] = useState<boolean>();
+
+useEffect(() => {
+  const fetchData = async () =>{
+    setisLoading(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    setRobotGallery(data);
+    setisLoading(false);
+
+  }
+
+  fetchData();
+},[])
+
   
-  }
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(data => this.setState({ robotGallery: data}));
-  }
-
-
-  render(){
     return (
       <div className={styles.app}>
         <div className={styles.appHeader}>
@@ -36,12 +36,14 @@ class App extends React.Component<Props, State> {
           <h1>robots are so cool!</h1>
           </div>
           <ShoppingCart />
-        <div className={styles.robotList}>
-        {robots.map(robotGallery => <Robot id={robotGallery.id} name={robotGallery.name} email={robotGallery.email} />)}
-        </div>
+       {!isLoading ? (<div className={styles.robotList}>
+        {robotGallery.map((r:any) => (
+            <Robot id={r.id} email={r.email} name={r.name} />
+          ))}
+        </div>): <h1>Loading</h1>}
       </div>
     );
-  }
+  
 
 
 }
